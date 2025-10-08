48771d8272f9f7e5effb4538dbfdd78b17614deb
Lachezar Kutsarov <lkutsarov@gmail.com>
2025-10-08 16:30:11 +0200

    Create Solution.ts

---
+++ Solution.ts
@@ -0,0 +1,32 @@
+
+function majorityFrequencyGroup(input: string): string {
+    const ALPHABET_SIZE = 26;
+    const ASCII_LOWER_CASE_A = 97;
+    const frequencyLetters: number[] = new Array(ALPHABET_SIZE).fill(0);
+    for (let i = 0; i < input.length; ++i) {
+        ++frequencyLetters[input.codePointAt(i) - ASCII_LOWER_CASE_A];
+    }
+
+    const frequencyLettersToGroupSize: number[] = new Array(input.length + 1).fill(0);
+    for (let current of frequencyLetters) {
+        ++frequencyLettersToGroupSize[current];
+    }
+
+    let maxGroupSize = 0;
+    let frequencyMaxGroupSize = 0;
+    for (let frequency = frequencyLettersToGroupSize.length - 1; frequency >= 1; --frequency) {
+        if (maxGroupSize < frequencyLettersToGroupSize[frequency]) {
+            maxGroupSize = frequencyLettersToGroupSize[frequency];
+            frequencyMaxGroupSize = frequency;
+        }
+    }
+
+    const uniqueLetters: string[] = new Array();
+    for (let i = 0; i < ALPHABET_SIZE; ++i) {
+        if (frequencyLetters[i] === frequencyMaxGroupSize) {
+            uniqueLetters.push(String.fromCodePoint(ASCII_LOWER_CASE_A + i));
+        }
+    }
+
+    return uniqueLetters.join('');
+};
